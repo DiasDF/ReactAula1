b4f7271f4ff292e3528d1818eb10e0f07db80d03
Milton Dias <milton.soares@cbm.df.gov.br>
2023-08-02 17:43:43 -0300

    Aula 10 - useState, const [name e password] e onChange={(e) => setPassword(e.target.value)}

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,42 @@
+// Aula 10 useState
+/*
+No React JS, é possível pegar o valor de um input através do atributo "value={}" do componente.
+Para fazer isso utilizando um componente funcional, basta atribuir ao atributo "value" o valor desejado
+e utilizar o evento onChange para atualizar o valor do input quando ele for alterado pelo usuário.
+
+Em um app real o Formuário "Form" é enviado via ajax ou outro método para um lugar definido,
+(requisições síncronas e assíncronas SPA)
+Neste Exemplo em React, O e.preventDefault() interrompe o envio via Ajax e roda tudo que vem abaixo dele.
+
+Importante: Sem o e.preventDefault() tudo que for digitado no input é perdido.
+O e.preventDefault() mantém o conteúdo da última digitação no input.
+*/
+/*
+Sequência para usar o Hook useState:
+-DEVE OBRIGATORIAMENTE! Faz o ** import { useState } from "react" **
+-DEVE OBRIGATORIAMENTE! Criar as constantes: const [ name, setName ] = useState() que pode ter um valor inicial useState('Milton') 
+-DEVE OBRIGATORIAMENTE! Criar o onChange: onChange={(e) => setName(e.target.value)} 
+   IMPORTANTE! O setName(e.target.value) Atribui os valores do input as constantes name e password.
+-Pode Criar um console.log (function cadastrarUsuario(e) {}) L.28 para visualizar o novo valor no terminal a cada onChange.
+   O console.log Recebe os valores via "setName" do input name.
+-Ao atribuir um valor Default para useState('Matheus') deve acrescentar o campo value={name} no Form.
+*/
+
+import { useState } from "react"
+
 function Form() {
+
    function cadastrarUsuario(e) {
       e.preventDefault()
-      console.log("Cadastrou o usuario!")
+      // console.log(name) // Neste exemplo é utiliza apenas para mostrar o valor default de useState("Milton")
+      // console.log("Cadastrou o usuario!") // Forma simples.
+      // Abaixo: Forma interpolada ( com 2 dados) de uso do console.log OBS: Essa é uma forma de simular o envio do formulário.
+      console.log(`O Usuário ${name} foi cadastrado com a senha: ${password}`) 
    }
+   
+
+   const [name, setName] = useState()
+   const [password, setPassword] = useState()
 
    return (
       <div>
@@ -15,7 +49,8 @@ function Form() {
                   id="name"
                   name="name"
                   placeholder="Digite o seu nome."
-                  // value={name}
+                  // value={name} Só tem utilida quando useState("Milton") tem valor default.
+                  onChange={(e) => setName(e.target.value)}
                />
             </div>
             <div>
@@ -26,7 +61,7 @@ function Form() {
                   name="password"
                   placeholder="Digite sua senha."
                   // value="Cadastrar"
-                  // onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                />
             </div>
             <div>
@@ -38,9 +73,3 @@ function Form() {
 }
 
 export default Form
-/*
-Em um app ele vai enviar o Form via ajax para outro lugar definido, (requisições síncronas e assíncronas SPA)
-O e.preventDefault() interrompe a execução Ajax e roda tudo que vem abaixo dele.
-importante: Sem o e.preventDefault() tudo que for digitado no input é perdido.
-O e.preventDefault() mantém o conteúdo da última digitação no input.
-*/
