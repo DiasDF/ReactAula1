6c55b310765e835d30a99b23da131af73e8a48bf
Milton Dias <milton.soares@cbm.df.gov.br>
2023-08-30 18:18:55 -0300

    Aula 15, import googleFonts, React Icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,20 @@
-import { Link } from "react-router-dom"
-
-function Footer () {
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
+function Footer() {
    return (
-      <div className="Footer">
-      <footer>Footer</footer>
-      </div>
+      <footer className="Footer">
+         <ul>
+            <li className="social-list Facebook">
+               <FaFacebook />
+            </li>
+            <li className="social-list Instagram">
+               <FaInstagram />
+            </li>
+            <li className="social-list Youtube">
+               <FaYoutube />
+            </li>
+         </ul>
+         <h2>Nosso Footer</h2>
+      </footer>
    )
 }
 
